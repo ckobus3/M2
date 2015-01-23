5d7d57b95e38218fa670cd784ff20497d6d0edd1
smusa3 <smusa3@gatech.edu>
2015-01-23 18:27:22 -0500

    Add Person4 name

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -1,4 +1,4 @@
-package edu.gatech.oad.antlab.person;
+/*package edu.gatech.oad.antlab.person;*/
 
 /**
  *  A simple class for person 4
@@ -17,7 +17,7 @@ public class Person4 {
      * @param pname the person's real name
      */
   public Person4(String pname) {
-    name = pname;
+	  name = pname;
   }
     /**
      * This method should return a string
@@ -30,8 +30,15 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      char[] array = new char[input.length()];
+      for (int i = 0, n = input.length(); i < n; i++) {      
+    	  	char c = input.charAt(i);
+            char newChar = (char)(c+1);
+            System.out.println(newChar);
+            array[i] = newChar;
+        }
+     
+      return new String(array);
     }
     
     /**
@@ -44,7 +51,18 @@ public class Person4 {
      */
     public String toString(String input) {
       return name + calc(input);
+      
     }
+    
+    public static void main(String[] args) {
+        Person4 person = new Person4("sarah");
+        String newTest = person.toString("abc123");
+        String testCalc = person.calc("abc123");
+        System.out.println(testCalc);
+        System.out.println(newTest);
 
+     }
+    
+  
 }
 
